12dd7e5d110510e198ed767033990b65e0d57f83
Andy Harless <a23@harless.us>
2019-05-26 21:59:24 -0400

    aggregated tweets from multiple streams

---
+++ utils.py
@@ -0,0 +1,58 @@
+# Utilities for Tweet Demographics Project
+
+
+
+# From https://gist.github.com/timothyrenner/dd487b9fd8081530509c
+
+#Gets the text, sans links, hashtags, mentions, media, and symbols.
+def get_text_cleaned(tweet):
+    text = tweet['text']
+    
+    slices = []
+    #Strip out the urls.
+    if 'urls' in tweet['entities']:
+        for url in tweet['entities']['urls']:
+            slices += [{'start': url['indices'][0], 'stop': url['indices'][1]}]
+    
+    #Strip out the hashtags.
+    if 'hashtags' in tweet['entities']:
+        for tag in tweet['entities']['hashtags']:
+            slices += [{'start': tag['indices'][0], 'stop': tag['indices'][1]}]
+    
+    #Strip out the user mentions.
+    if 'user_mentions' in tweet['entities']:
+        for men in tweet['entities']['user_mentions']:
+            slices += [{'start': men['indices'][0], 'stop': men['indices'][1]}]
+    
+    #Strip out the media.
+    if 'media' in tweet['entities']:
+        for med in tweet['entities']['media']:
+            slices += [{'start': med['indices'][0], 'stop': med['indices'][1]}]
+    
+    #Strip out the symbols.
+    if 'symbols' in tweet['entities']:
+        for sym in tweet['entities']['symbols']:
+            slices += [{'start': sym['indices'][0], 'stop': sym['indices'][1]}]
+    
+    # Sort the slices from highest start to lowest.
+    slices = sorted(slices, key=lambda x: -x['start'])
+    
+    #No offsets, since we're sorted from highest to lowest.
+    for s in slices:
+        text = text[:s['start']] + text[s['stop']:]
+        
+    return text
+
+
+
+def is_male_g(gd,name):
+    if len(name)>0:
+        return(gd.get_gender(name.split()[0])=='male')
+    else:
+        return(False)
+
+def is_female_g(gd,name):
+    if len(name)>0:
+        return(gd.get_gender(name.split()[0])=='female')
+    else:
+        return(False)
